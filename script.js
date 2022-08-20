11f7ae660899c067f546a2f22d5145776596022b
Bảo Bùi <100954210+BuiBao3103@users.noreply.github.com>
2022-08-20 19:14:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
         }
     });
     $('.form-submit').click(function(){
-        window.open('home.html');
+        window.location.pathname='https://buibao3103.github.io/KT_SOCIAL/home.html';
     });
 });
 
