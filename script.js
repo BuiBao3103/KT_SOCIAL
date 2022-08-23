9f0689921f766f235661a55ef47cf60ef8be7663
BuiBao3103 <hongbao2003@gmail.com>
2022-08-23 10:39:25 +0700

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Login
 $(document).ready(function(){
     $('#eye').click(function(){
         $(this).toggleClass('open');
@@ -8,41 +9,48 @@ $(document).ready(function(){
             $(this).prev().attr('type', 'password');
         }
     });
-    $('.form-submit').click(function(){
-        window.open('https://buibao3103.github.io/KT_SOCIAL/home.html');
+    $(window).scroll(function(){
+        if($(this).scrollTop())
+        {
+           
+            $('header').addClass('sticky');
+        }
+        else
+        {
+            $('header').removeClass('sticky');
+        }
     });
 });
 
-
-let slideIndex = 1;
-
+// Slider
+let slideIndex = 0;
 autoshowSlides();
-
 function autoshowSlides() {
     let i;
     let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("active", "");
     }
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
-    setTimeout(autoshowSlides, 4000); // Change image every 2 seconds
+    dots[slideIndex-1].className += " active";
+    setTimeout(autoshowSlides, 2000); // Change image every 2 seconds
 }
-
-
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
      showSlides(slideIndex += n);
 }
-
 // Thumbnail image controls
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+    showSlides(slideIndex += n);
 }
-
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
@@ -53,40 +61,39 @@ function showSlides(n) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+        dots[i].className = dots[i].className.replace("active", "");
     }
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
 }
 
+//Story
 let storyIndex = 0;
-function plusStories(n, direction) {
-    showStory(storyIndex += n, direction);
-}
-
-function showStory(n, direction) {
-    console.log(storyIndex)
+function showStory(direction) {
     let stories = document.getElementsByClassName("myStory");
     console.log(stories.length)
     if (storyIndex < 0){
-        storyIndex += 1;
+        storyIndex = 0;
         return;
     }
     if (storyIndex == stories.length){
-        storyIndex -= 1;
+        storyIndex = stories.length -1 ;
         return;
     }
     if(direction == 'right'){
+        storyIndex++;
         stories[storyIndex-1].style.display = "none";
         
     }
     else {
+        storyIndex--;
         stories[storyIndex].classList.add("animate__animated");
         stories[storyIndex].classList.add("animate__slideInLeft");
         stories[storyIndex].style.display = "block";
     }
 }
 
+//market prodiuct
 function market(type,element)
 {
     let tabs = document.getElementsByClassName('item');
@@ -117,130 +124,88 @@ function market(type,element)
             break;
     }
 }
+
+//messenge info boxchat
 function giaodien(type,element)
 {
-    console.log(type);
-    if (pc.matches && type != 'nen1') { // If media query matches
+    if (mobile.matches) { // If media query matches
         let mess = document.querySelector(".mess_er");
         mess.style.display='none';
     }
-    let tabs = document.getElementsByClassName('gd');
-    for(i=0;i<tabs.length;i++)
-    {
-        tabs[i].style.background='#AB9383';
-    }
-    if (type !='nen1')
+    else
+   {
+        let tabs = document.getElementsByClassName('gd');
+        for(i=0;i<tabs.length;i++)
+        {
+            tabs[i].style.background='#AB9383';
+        }
         element.style.background ='#daa48e';
+    }   
+
+    document.querySelector('.box_chat').style.display='none';
+    let khungtinnhan = document.querySelectorAll(".khungtinnhan");
+    for(let i=0;i<khungtinnhan.length;i++){
+        khungtinnhan[i].style.display='none';
+    }
 
     document.getElementById(type).style.display='block';
-    switch (type) {
-        case 'nen1':
-            document.getElementById('nen2').style.display='none';
-            document.getElementById('nen3').style.display='none';
-            document.getElementById('nen4').style.display='none';
-            document.getElementById('nen5').style.display='none';
-            document.getElementById('nen6').style.display='none';
-            break;
-        case 'nen2':
-            document.getElementById('nen1').style.display='none';
-            document.getElementById('nen3').style.display='none';
-            document.getElementById('nen4').style.display='none';
-            document.getElementById('nen5').style.display='none';
-            document.getElementById('nen6').style.display='none';
-            break;
-        case 'nen3':
-            document.getElementById('nen2').style.display='none';
-            document.getElementById('nen1').style.display='none';
-            document.getElementById('nen4').style.display='none';
-            document.getElementById('nen5').style.display='none';
-            document.getElementById('nen6').style.display='none';
-            break;
-        case 'nen4':
-            document.getElementById('nen2').style.display='none';
-            document.getElementById('nen3').style.display='none';
-            document.getElementById('nen1').style.display='none';
-            document.getElementById('nen5').style.display='none';
-            document.getElementById('nen6').style.display='none';
-            break;
-        case 'nen5':
-            document.getElementById('nen2').style.display='none';
-            document.getElementById('nen3').style.display='none';
-            document.getElementById('nen4').style.display='none';
-            document.getElementById('nen1').style.display='none';
-            document.getElementById('nen6').style.display='none';
-            break;
-        case 'nen6':
-            document.getElementById('nen2').style.display='none';
-            document.getElementById('nen3').style.display='none';
-            document.getElementById('nen4').style.display='none';
-            document.getElementById('nen5').style.display='none';
-            document.getElementById('nen1').style.display='none';
-            break;
-        default:
-            break;
-    }
 }
 
-
-
-
+//messenge mobile box chat back member
 function back(){
     let khungtinnhan = document.querySelectorAll(".khungtinnhan");
-    console.log(khungtinnhan)
     for(let i=0;i<khungtinnhan.length;i++){
         khungtinnhan[i].style.display='none';
     }
     mess=document.querySelector(".mess_er");
     mess.style.display='block';
 }
+
+//market uncheck all checkbox
 function UnCheckAll(){
+    let list_input = document.querySelectorAll('#market > #menu-option input[type="text"]');
+    for (let i=0; i < list_input.length;i++){
+        list_input[i].value = "";
+    }
     let list_checkbox = document.querySelectorAll('input[type="checkbox"]');
-    console.log(list_checkbox);
-    for(var i=0;i<list_checkbox.length;i++){
+    for(let i=0;i<list_checkbox.length;i++){
         list_checkbox[i].checked = false;
     }
+    
 }
+
+//nofication manager
 function showNofi(curr_page)
 {
-    bell = document.querySelector('.js-bell')
-    nofication = document.querySelector('#nofication')
-    head = document.getElementById("header");
-    if (!pc.matches ) { // If media query matches
+    let nofication = document.querySelector('#nofication');
+    let head = document.getElementById("header");
+    if (!mobile.matches ) { // If media query matches
         head.classList.remove("animate__slideInDown");
-        head.classList.add("animate__animated", "animate__slideOutUp");
+        head.classList.add("animate__animated");
+        head.classList.add("animate__slideOutUp");
     }
     else{
+        document.getElementById('slider').style.display= 'block';
+        
+        let tabs = document.querySelectorAll('.tab-item > a');
+        for(let tab of tabs){
+            tab.style.backgroundColor = '#fff';
+        }
+        document.querySelector('.js-bell').style.backgroundColor = "#9E663B";
         document.getElementById(curr_page).style.display='none';
     }
-    
     nofication.classList.add('open');
-    console.log("bell");
 
 }
 function hideNofi()
 {
+    let nofication = document.querySelector('#nofication');
     nofication.classList.remove('open');
-    head = document.getElementById("header");
-    if (!pc.matches ) { // If media query matches
+    let head = document.getElementById("header");
+    if (!mobile.matches ) { // If media query matches
         head.classList.remove("animate__slideOutUp");
         head.classList.add("animate__slideInDown")
     }
 }
 
-var pc = window.matchMedia("(max-width: 1024px)")
-
-
-$(document).ready(function(){
-    $('#eye').click(function(){
-        $(this).toggleClass('open');
-        $(this).children('i').toggleClass('fa-eye-slash fa-eye');
-        if($(this).hasClass('open')){
-            $(this).prev().attr('type', 'text');
-        }else{
-            $(this).prev().attr('type', 'password');
-        }
-    });
-    $('.form-submit').click(function(){
-        window.open('home.html');
-    });
-});
+var mobile = window.matchMedia("(max-width: 1024px)")
